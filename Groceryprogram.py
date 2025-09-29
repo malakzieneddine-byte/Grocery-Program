776e27196bcd0f0370ea5df7c40eb5945327e5e4
Malak <malak.zieneddine@gmail.com>
2025-09-29 21:29:53 +0300

    adding items to shipping cart

--- Groceryprogram.py
+++ Groceryprogram.py
@@ -1,1 +1,19 @@
-print("malak")
+groceries = {
+    "apple": 2,
+    "banana": 1,
+    "milk": 3,
+    "bread": 2
+}
+cart=[]
+
+item = ""
+
+while (item!= "done"):
+    item = input("What do you want to buy(type done when you finish): ")
+    if (item in groceries):
+        cart.append(item)
+    else:
+       if (item!="done"):
+        print("Sorry,  we don’t have that item")    
+
+
